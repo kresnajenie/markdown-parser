b8354a2f5b5f774c5208574e23aaffa933601321
Kresna Jenie <kresna.jenie@gmail.com>
2022-04-15 17:23:24 -0700

    Fixed no square bracket error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,13 +16,18 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             if (currentIndex == markdown.length()-1) {
                 break;
             }
+            if (openBracket == -1) {
+                break;
+            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+
+            
         }
-        
+
         return toReturn;
     }
 
@@ -31,6 +36,6 @@ public class MarkdownParse {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
     }
 }
