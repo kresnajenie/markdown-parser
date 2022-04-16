f999e3e7d57ed0c7772209c4ec1d97d9ba44aa91
Kresna Jenie <kresna.jenie@gmail.com>
2022-04-15 17:06:20 -0700

    Fixed new line error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,14 +12,15 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int test = markdown.indexOf("[");
-            System.out.println(test);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            if (currentIndex == markdown.length()-1) {
+                break;
+            }
         }
 
         return toReturn;
